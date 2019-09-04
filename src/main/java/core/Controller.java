38190e98206397a9401639cb863ce5612bac580c
ryounger <reecechimento@gmail.com>
2019-09-04 09:43:44 -0500

    Add Controller JavaDoc, format switch cases

--- src/main/java/core/Controller.java
+++ src/main/java/core/Controller.java
@@ -1,5 +1,11 @@
 package core;
 
+/**
+ * The Controller class specifies how the user may interact with the 
+ * student-course registration model.
+ * 
+ * @author Reece Youngert a
+ */
 public class Controller
 {
     /** The Model encapsulates the student, course, and registration models. */
@@ -28,63 +34,81 @@ public class Controller
             
             case "student":
             switch (action) {
+
             case "create":
                 // TODO student create
                 break;
+
             case "read":
                 // TODO student read
                 break;
+
             case "update":
                 // TODO student update
                 break;
+
             case "delete":
                 // TODO student delete
                 break;
             default:
                 // TODO: student error
+                // TODO: student action help
                 break;
             }
 
             case "course":
             switch (action) {
+
             case "create":
                 // TODO course create
                 break;
+
             case "read":
                 // TODO course read
                 break;
+
             case "update":
                 // TODO course update
                 break;
+
             case "delete":
                 // TODO course delete
                 break;
+
             default:
                 // TODO: course error
+                // TODO: course action help
                 break;
             }
 
             case "registration":
             switch (action) {
+
             case "create":
                 // TODO registration create
                 break;
+
             case "read":
                 // TODO registration read
                 break;
+
             case "update":
                 // TODO registration update
                 break;
+
             case "delete":
                 // TODO registration delete
                 break;
+
             default:
                 // TODO: registration error
+                // TODO registration action help
                 break;
             }
 
             default:
                 // TODO: model error
+                // TODO: model help
                 break;
         } // end switch(model)
 
