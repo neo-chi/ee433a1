6e03ffef81da927f3851db1f8790c3beb158fe67
ryounger <reecechimento@gmail.com>
2019-09-04 08:17:23 -0500

    Initialzed Controller template

---
+++ src/main/java/core/Controller.java
@@ -0,0 +1,93 @@
+package core;
+
+public class Controller
+{
+    /** The Model encapsulates the student, course, and registration models. */
+    protected Model model;
+
+    /**
+     * Returns a Controller object that allows the user to interact with the 
+     * student-course registration model.
+     */
+    public Controller()
+    {
+        this.model = new Model();
+    }
+
+    /**
+     * Given a model, action, and parameters, performs the desired task on a
+     * student, course, or registration model.
+     * 
+     * @param model the name of the model.
+     * @param action the name of the action.
+     * @param parameters the parameters for the given action.
+     */
+    public void handleRequest(String model, String action, String parameters)
+    {
+        switch (model) {
+            
+            case "student":
+            switch (action) {
+            case "create":
+                // TODO student create
+                break;
+            case "read":
+                // TODO student read
+                break;
+            case "update":
+                // TODO student update
+                break;
+            case "delete":
+                // TODO student delete
+                break;
+            default:
+                // TODO: student error
+                break;
+            }
+
+            case "course":
+            switch (action) {
+            case "create":
+                // TODO course create
+                break;
+            case "read":
+                // TODO course read
+                break;
+            case "update":
+                // TODO course update
+                break;
+            case "delete":
+                // TODO course delete
+                break;
+            default:
+                // TODO: course error
+                break;
+            }
+
+            case "registration":
+            switch (action) {
+            case "create":
+                // TODO registration create
+                break;
+            case "read":
+                // TODO registration read
+                break;
+            case "update":
+                // TODO registration update
+                break;
+            case "delete":
+                // TODO registration delete
+                break;
+            default:
+                // TODO: registration error
+                break;
+            }
+
+            default:
+                // TODO: model error
+                break;
+        } // end switch(model)
+
+    } // end handleRequest()
+
+}
